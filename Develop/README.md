bd5d7a6f8946d6512f1885391ab1cd6f58daac69
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-07-14 20:20:07 -0700

    Had to restore files

---
+++ Develop/README.md
@@ -0,0 +1,71 @@
+
+
+  # one 
+
+    LGPL License
+    
+---
+
+## Contents
+
+  1. [Description](#description)
+         [User Stroy](#user-story)
+         [Acceptance Criteria](#acceptance-criteria)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [Credit](#credit)
+  5. [License](#badges)
+  6. [Contribution](#contributing)
+  7. [Tests](#tests)
+  8. [Questions](#questions)
+
+---  
+## Description
+
+  two  
+
+    User Story  
+
+      three  
+
+    Acceptance Criteria  
+
+      four
+      
+---
+## Installation
+
+  five
+
+---
+## Usage
+
+  six
+
+---
+##  Credit
+
+  seven
+
+---
+##  License
+
+  LGPL License
+---
+##  Contribution
+
+  ok
+
+---
+##  Tests
+
+  test
+
+---
+##  Questions
+
+      Contact me at:  
+  
+        E-mail: you  
+        Github: me  
+        LinkedIn: ok 
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,75 @@
+generateMardown = (data) => {
+  return `
+
+  # ${data.title}
+
+    ${data.badges}
+    
+---
+
+## Contents
+
+  1. [Description](#description)
+         [User Stroy](#user-story)
+         [Acceptance Criteria](#acceptance-criteria)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [Credit](#credit)
+  5. [License](#badges)
+  6. [Contribution](#contributing)
+  7. [Tests](#tests)
+  8. [Questions](#questions)
+
+---  
+## Description
+
+  ${data.description}  
+
+    User Story  
+
+      ${data.userStory}  
+
+    Acceptance Criteria  
+
+      ${data.acceptanceCriteria}
+      
+---
+## Installation
+
+  ${data.installation}
+
+---
+## Usage
+
+  ${data.usage}
+
+---
+##  Credit
+
+  ${data.credit}
+
+---
+##  License
+
+  ${data.badges}
+---
+##  Contribution
+
+  ${data.contributing}
+
+---
+##  Tests
+
+  ${data.test}
+
+---
+##  Questions
+
+      Contact me at:  
+  
+        E-mail: ${data.email}  
+        Github: ${data.github}  
+        LinkedIn: ${data.linkedin} ` 
+}
+
+  module.exports = generateMardown;
